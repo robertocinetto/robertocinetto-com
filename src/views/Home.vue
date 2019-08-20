17a020e393f4086685e2e9798e53495a3f456691
Roberto Cinetto <roberto.cinetto@gmail.com>
2019-08-20 10:00:57 +0200

    porting sito statico

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="home container">
+    <div class="row">
+      <div class="col-sm-12 d-flex justify-content-center align-items-center flex-column">
+        <img class="img-fluid" id="logo" src="@/assets/logo-roberto-cinetto.svg" alt="">
+        <div class="contact d-flex  align-items-center flex-column">
+          <span>email: <a class="email" href="mailto:roberto.cinetto@gmail.com">roberto.cinetto@gmail.com</a></span>
+          <span>tel: <a class="tel" href="tel:+393494955483">349 49 55 483</a></span>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
   }
 }
 </script>
