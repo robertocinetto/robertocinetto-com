98e7585827bd8bba9930165b76bd107a6b6c52ba
Roberto Cinetto <roberto.cinetto@gmail.com>
2020-01-27 08:48:54 +0100

    vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "Roberto Cinetto - Blog",
+  description: "My VuePress powered docs",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" },
+      { text: "Author", link: "https://pmbanugo.me" }
+    ],
+    sidebar: ["/guide/", "/guide/getting-started"]
+  }
+};
